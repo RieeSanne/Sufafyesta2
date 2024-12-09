3036fadec3189104febb42bfa4a5e2f54e9b2bfc
Aleah11 <165635641+Aleah11@users.noreply.github.com>
2024-12-09 13:49:55 +0800

    Win Lose Interaction and bruh nalimtan ko pero gn islan ko pgd salog ka habulan :D

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);}
+
+    public void Main (){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 5);}
+
+    public void Town(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);}
+
+     public void Retry (){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);}
+
+    public void Town1(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);}
+
+    public void QuitGame(){
+        Debug.Log ("Quit!");
+        Application.Quit();
+    }
+}
